ba39ea45bfcc836446ac1aaf8686e20df1af7ac2
Alex Kiura <kiuraalex@gmail.com>
2021-02-23 13:57:36 +0300

    Add Todo item

---
+++ src/todos/TodoItem.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import { useDispatch, useTrackedState } from '../store';
+import { useFlasher } from '../utils';
+
+
+const renderHighlight = (title, query) => {
+    if (!query) return title;
+    const index = title.indexOf(query);
+    if (index === -1) return title;
+    return (
+      <React.Fragment>
+        {title.slice(0, index)}
+        <b>{query}</b>
+        {title.slice(index + query.length)}
+      </React.Fragment>
+    );
+};
+
+
+const TodoItem = ({ id, title, completed }) => {
+    const dispatch = useDispatch();
+    const state = useTrackedState();
+    const delTodo = () => {
+        dispatch({ type: 'DELETE_TODO', id});
+    };
+    return (
+        <li ref={useFlasher}>
+            <input
+                type="checkbox"
+                cehcked={!!completed}
+                onChange={() => dispatch({ type: 'TOGGLE_TOO', id })}
+            />
+            <span
+                style={{
+                    textDecoration: completed? 'line-through' : 'none',
+                }}
+            >
+                {completed ? title : renderHighlight(title, state.query)}
+            </span>
+            <button onClick={delTodo}> Delete </button>
+
+        </li>
+    );
+};
+
+export default React.memo(TodoItem);
+
+
+
+
