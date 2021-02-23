be6716475146ec8d07dfdaa8cfc49b425155a58a
Alex Kiura <kiuraalex@gmail.com>
2021-02-23 20:44:37 +0300

    Refactor TodoList to use hooks

--- src/components/todos/TodoList.js
+++ src/components/todos/TodoList.js
@@ -1,28 +1,26 @@
 import React from 'react';
-import { useDispatch, useTrackedState} from '../../store';
 
+import { useTodoList } from '../../hooks/useTodoList';
+import { useQuery } from '../../hooks/useQuery';
 import TodoItem from './TodoItem';
 import NewTodo from './NewTodo';
 
 
 const TodoList = () => {
-    const dispatch = useDispatch();
-    const state = useTrackedState();
-    const setQuery = event => {
-        dispatch({ type: 'SET_QUERY', query: event.target.value});
-    };
+    const { getQuery, setQuery } = useQuery();
+    const todos = useTodoList();
 
     return (
         <div>
             <ul>
-                {state.todos.map(({id, title, completed}) => (
+                {todos.map(({id, title, completed}) => (
                     <TodoItem key={id} id={id} title={title} completed={completed} />
                 ))}
                 <NewTodo />
             </ul>
             <div>
                 Highlight Query for incomplete items:
-                <input value={state.query} onChange={setQuery} />
+                <input value={getQuery()} onChange={e => setQuery(e.target.value)} />
             </div>
         </div>
     );
